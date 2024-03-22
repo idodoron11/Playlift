945d1d0a2cc29b4284e07e2dc31604b95694a150
Ido Doron <ido.doron@gmail.com>
2024-03-22 23:10:04 +0200

    Remove unused code

--- matchers/spotify_matcher.py
+++ matchers/spotify_matcher.py
@@ -37,7 +37,6 @@ class SpotifyMatcher(Matcher):
     def match_list(self, tracks: Iterable[Track]) -> List[Iterable[SpotifyTrack]]:
         tracks = list(tracks)
         sp_tracks: List[Iterable[SpotifyTrack]] = []
-        guessed_tracks_positions: List[(int, Iterable[SpotifyTrack])] = []
         for index, track in enumerate(tqdm(tracks)):
             match = self.match(track)
             if match:
@@ -46,18 +45,4 @@ class SpotifyMatcher(Matcher):
             suggestions = self.suggest_match(track)
             if suggestions:
                 sp_tracks.append(suggestions)
-
-        # if len(guessed_tracks_positions) > 0:
-        #     print("The following tracks were guessed:\n")
-        #     for index, _ in guessed_tracks_positions:
-        #         source: Track = tracks[index]
-        #         target: SpotifyTrack = sp_tracks[index]
-        #         print(
-        #             f"{index + 1} {source.display_artist} - {source.title}, {source.album}    --->    {target.display_artist} - {target.title}, {target.album}")
-        #     print("\nIf any of the tracks above were not mapped correctly, you have an opportunity to remove them")
-        #     indices_str = click.prompt("Enter all the indices of tracks you wish to remove, separated by comma", default="")
-        #     if len(indices_str.strip()) > 0:
-        #         indices = set(map(lambda index_str: int(index_str.strip()) - 1, indices_str.split(",")))
-        #         return [track for index, track in enumerate(sp_tracks) if index not in indices]
-
         return sp_tracks
