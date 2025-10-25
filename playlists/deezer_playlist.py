63c8301c1f10937808b63580bb893841fa5ca5b2
Ido Doron <ido.doron@gmail.com>
2025-10-25 13:32:14 +0300

    Refactor DeezerPlaylist and DeezerTrack classes to add type hints and improve method signatures

--- playlists/deezer_playlist.py
+++ playlists/deezer_playlist.py
@@ -1,3 +1,4 @@
+from typing import Optional, Any, Iterator
 import re
 import deezer
 from tracks.deezer_track import DeezerTrack
@@ -6,12 +7,12 @@ from config import CONFIG
 class DeezerPlaylist:
     PLAYLIST_URL_PATTERN = r'https://www\.deezer\.com(?:/[a-z]{2})?/playlist/(\d+)'
 
-    def __init__(self, playlist_id_or_url):
-        self._client = None
-        self._data = None
-        self.playlist_id = self._get_playlist_id(playlist_id_or_url)
+    def __init__(self, playlist_id_or_url: str | int) -> None:
+        self._client: Optional[deezer.Client] = None
+        self._data: Optional[Any] = None
+        self.playlist_id: int = self._get_playlist_id(playlist_id_or_url)
 
-    def _get_playlist_id(self, playlist_id_or_url: int | str) -> int:
+    def _get_playlist_id(self, playlist_id_or_url: str | int) -> int:
         # Handle URLs by extracting the ID
         if isinstance(playlist_id_or_url, str) and playlist_id_or_url.startswith('http'):
             match = re.match(self.PLAYLIST_URL_PATTERN, playlist_id_or_url)
@@ -32,29 +33,29 @@ class DeezerPlaylist:
         else:
             raise ValueError("playlist_id_or_url must be an integer ID or a valid Deezer playlist URL")
 
-    def _client_instance(self):
+    def _client_instance(self) -> deezer.Client:
         if self._client is None:
             self._client = deezer.Client()
         return self._client
 
-    def _ensure_data(self, force=False):
+    def _ensure_data(self, force: bool = False) -> None:
         if self._data is None or force:
             client = self._client_instance()
             self._data = client.get_playlist(self.playlist_id)
 
     @property
-    def name(self):
+    def name(self) -> str:
         self._ensure_data()
         return self._data.title
 
     @property
-    def tracks(self):
+    def tracks(self) -> Iterator[DeezerTrack]:
         self._ensure_data()
         for track in self._data.tracks:
             yield DeezerTrack(track.id)
 
     @staticmethod
-    def create(name):
+    def create(name: str) -> 'DeezerPlaylist':
         client = deezer.Client()
         # Get the current user and create a playlist
         user = client.get_user()
@@ -65,7 +66,7 @@ class DeezerPlaylist:
         result._data = playlist  # Set initial data to avoid an extra API call
         return result
 
-    def add_tracks(self, tracks):
+    def add_tracks(self, tracks: list[DeezerTrack]) -> bool:
         """Add tracks to the playlist."""
         track_ids = [int(track.track_id) for track in tracks]
         if not track_ids:
@@ -81,5 +82,5 @@ class DeezerPlaylist:
             self._ensure_data(force=True)
         return success
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Deezer Playlist: {self.name} ({self.playlist_id})"
--- tracks/deezer_track.py
+++ tracks/deezer_track.py
@@ -1,43 +1,44 @@
+from typing import Optional, Any
 import deezer
 
 class DeezerTrack:
-    def __init__(self, track_id):
+    def __init__(self, track_id: str | int) -> None:
         self.track_id = str(track_id)
-        self._data = None
-        self._client = None
+        self._data: Optional[Any] = None
+        self._client: Optional[deezer.Client] = None
 
-    def _client_instance(self):
+    def _client_instance(self) -> deezer.Client:
         if self._client is None:
             self._client = deezer.Client()
         return self._client
 
-    def _ensure_data(self):
+    def _ensure_data(self) -> None:
         if self._data is None:
             client = self._client_instance()
             self._data = client.get_track(self.track_id)
 
     @property
-    def title(self):
+    def title(self) -> str:
         self._ensure_data()
         return self._data.title
 
     @property
-    def artist(self):
+    def artist(self) -> str:
         self._ensure_data()
         return self._data.artist.name
 
     @property
-    def album(self):
+    def album(self) -> str:
         self._ensure_data()
         return self._data.album.title
 
     @property
-    def duration(self):
+    def duration(self) -> int:
         self._ensure_data()
         return self._data.duration
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.track_id
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<DeezerTrack {self.track_id}>"
